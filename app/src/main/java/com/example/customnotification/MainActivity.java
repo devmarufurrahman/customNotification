f7c0246713415b5c432ccd47c25375e751e25e91
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-08-23 09:46:21 +0600

    not working

--- app/src/main/java/com/example/customnotification/MainActivity.java
+++ app/src/main/java/com/example/customnotification/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.core.content.res.ResourcesCompat;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -14,6 +16,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     private  static final  String CHANNEL_ID = "My Channel";
     private  static final  int NOTIFICATION_ID = 100;
+    private static final int REQ_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +32,40 @@ public class MainActivity extends AppCompatActivity {
 
         Notification notification;
         NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        Intent iNotify = new Intent(getApplicationContext(), MainActivity.class);
+        iNotify.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pi = PendingIntent.getActivity(this,REQ_CODE,iNotify,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Big picture Style
+        Notification.BigPictureStyle bigPictureStyle = new Notification.BigPictureStyle()
+                .bigPicture(((BitmapDrawable)(ResourcesCompat.getDrawable(getResources(),R.drawable.notification, null))).getBitmap())
+                .bigLargeIcon(largeIcon)
+                .setBigContentTitle("Image sent by maruf")
+                .setSummaryText("Image Message");
+
+//        Inbox style
+        Notification.InboxStyle inboxStyle = new Notification.InboxStyle()
+                .addLine("A")
+                .addLine("b")
+                .addLine("c")
+                .addLine("d")
+                .addLine("e")
+                .addLine("f")
+                .addLine("g")
+                .setBigContentTitle("Full Message")
+                .setSummaryText("Message from Maruf");
+
+
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             notification = new Notification.Builder(this)
                     .setLargeIcon(largeIcon)
                     .setSmallIcon(R.drawable.notification)
                     .setContentText("New Notification")
                     .setSubText("Your new notification is arrival")
+                    .setContentIntent(pi)
+                    .setStyle(bigPictureStyle)
                     .setChannelId(CHANNEL_ID)
                     .build();
             nm.createNotificationChannel(new NotificationChannel(CHANNEL_ID,"New Channel", NotificationManager.IMPORTANCE_HIGH));
@@ -44,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
                     .setSmallIcon(R.drawable.notification)
                     .setContentText("New Notification")
                     .setSubText("Your new notification is arrival")
+                    .setStyle(bigPictureStyle)
+                    .setContentIntent(pi)
                     .build();
         }
 
